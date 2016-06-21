6875bf343d3590cd8604cf289318e8ed4f039536
chris.zh <chris.zh@outlook.com>
2016-06-21 10:51:04 +0800

    a'

--- src/main/java/com/qiandaibaobao/util/Utils.java
+++ src/main/java/com/qiandaibaobao/util/Utils.java
@@ -8,10 +8,11 @@ import java.io.UnsupportedEncodingException;
 public class Utils {
     /**
      * 字符串转换成UTF-8
+     *
      * @param str
      * @return
      */
-    public static String utf8String(String str){
+    public static String utf8String(String str) {
         String newStr = null;
         try {
             newStr = new String(str.getBytes("UTF-8"));
@@ -19,9 +20,5 @@ public class Utils {
             e.printStackTrace();
         }
         return newStr;
-    }
-
-    public static void main(String[] args){
-        String certiType = "";
     }
 }
--- src/main/resources/initDb.sql
+++ src/main/resources/initDb.sql
@@ -4,3 +4,11 @@ id int unsigned not null auto_increment primary key,
 name text not null,
 password text not null
 );
+
+
+--oracle版本
+create table tmp_user(
+id number,
+name varchar2(100),
+password varchar2(100)
+);
