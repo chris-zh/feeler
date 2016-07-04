fb8751628126df646d37ead1ead090eb991a20c2
chris.zh <chris.zh@outlook.com>
2016-07-04 13:40:35 +0800

    delete trush

--- src/main/java/com/qiandaibaobao/controller/UserController.java
+++ src/main/java/com/qiandaibaobao/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;p
 import javax.servlet.http.HttpSession;
 
 /**
