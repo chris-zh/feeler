d0fb5ce15bd17c8d469e0c4b6ae75ab2ff0d3ca6
chris.zh <chris.zh@outlook.com>
2016-08-04 20:22:59 +0800

    test

--- src/test/java/com/qiandaibaobao/test/LoginTest.java
+++ src/test/java/com/qiandaibaobao/test/LoginTest.java
@@ -1,7 +1,11 @@
 package com.qiandaibaobao.test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.TimeZone;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -9,9 +13,16 @@ import com.google.gson.JsonObject;
 
 public class LoginTest{
 	
-	public static void main(String[] args) {
-		Thread th1 = new Thread();
-		th1.start();
-	}
+	public static void main(String[] args) throws ParseException {
+		String format = "yyyyMMdd";
+		String date = "19890416";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+		java.util.Date date2 = simpleDateFormat.parse(date);
+        System.out.println("TimeZone.getDefault() = " + TimeZone.getDefault());
+//        TimeZone.setDefault(TimeZone.getTimeZone("America/Los_Angeles"));
+//        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        System.out.println("date2 = " + date2);
+
+    }
 
 }
